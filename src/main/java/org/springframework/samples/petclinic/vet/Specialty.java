d150522d6cae566c3fd2aa3a4d4585f90351ba3f
davidzarandieta <davzarort@alum.us.es>
2023-02-27 19:24:00 +0100

    prueba

--- src/main/java/org/springframework/samples/petclinic/vet/Specialty.java
+++ src/main/java/org/springframework/samples/petclinic/vet/Specialty.java
@@ -15,7 +15,7 @@
  */
 package org.springframework.samples.petclinic.vet;
 
-import javax.persistence.Entity;
+import javax.persistence.Entity; 
 import javax.persistence.Table;
 
 import org.springframework.samples.petclinic.model.NamedEntity;
