d382c60206154f66ddb0db2fffa7d16d4b0b44ec
Pedro González Marcos <pedgonmar2@alum.us.es>
2023-03-11 18:25:50 +0100

    feat: añadida validación reservas concurrentes
    Se han quitado una anotación innecesaria en el reposiotorio y se ha
    añdido un método que devuelve las reservas de las mascotas. Se ha
    añadido la lógica subyacente para validar las reservas concurrentes
    de mascotas

--- src/main/java/org/springframework/samples/petclinic/pethotel/PetHotelService.java
+++ src/main/java/org/springframework/samples/petclinic/pethotel/PetHotelService.java
@@ -1,5 +1,7 @@
 package org.springframework.samples.petclinic.pethotel;
 
+import java.util.Collection;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Service;
@@ -8,26 +10,46 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 public class PetHotelService {
 
-	protected PetHotelRepository petHotelRepository;
-	
-	@Autowired
-	PetHotelService(PetHotelRepository petHotelRepository) {
-		this.petHotelRepository = petHotelRepository;
-	}
-	
-	@Transactional(readOnly = true)
-	public PetHotel getPetHotelById(Integer id) throws DataAccessException {
-		return petHotelRepository.findById(id).get();
-	}
-	
-	@Transactional 
-	public void save(PetHotel pt) throws DataAccessException {
-		petHotelRepository.save(pt);
-	}
-	
-	@Transactional
-	public void deletePetHotel(Integer id) throws DataAccessException {
-		petHotelRepository.delete(petHotelRepository.findById(id).get());
-	}
-	
+    private final PetHotelRepository petHotelRepository;
+
+    @Autowired
+    PetHotelService(PetHotelRepository petHotelRepository) {
+        this.petHotelRepository = petHotelRepository;
+    }
+
+    @Transactional(readOnly = true)
+    public PetHotel getPetHotelById(Integer id) throws DataAccessException {
+        return petHotelRepository.findById(id).get();
+    }
+
+    @Transactional(readOnly = true)
+    public Collection<PetHotel> getPetHotelByPetId(Integer petId) throws DataAccessException {
+        return petHotelRepository.findBookingsByPetId(petId);
+    }
+
+    @Transactional
+    public void save(PetHotel petHotel) throws DataAccessException {
+        Collection<PetHotel> bookings = getPetHotelByPetId(petHotel.getPet().getId());
+        if (!concurrentBookingsPresent(bookings, petHotel)) {
+            petHotelRepository.save(petHotel);
+        }
+    }
+
+    @Transactional
+    public void deletePetHotel(Integer id) throws DataAccessException {
+        petHotelRepository.delete(petHotelRepository.findById(id).get());
+    }
+
+    public boolean concurrentBookingsPresent(Collection<PetHotel> petBookings, PetHotel newBook) {
+        return petBookings.stream().anyMatch(book -> isBookingConcurrent(book, newBook));
+    }
+
+    public boolean isBookingConcurrent(PetHotel book, PetHotel newBook) {
+        if ((newBook.getStart().isBefore(book.getStart()) && newBook.getFinish().isBefore(book.getStart()))
+                || (book.getFinish().isBefore(newBook.getStart()) && book.getFinish().isBefore(newBook.getFinish()))) {
+            return false;
+        }
+        return true;
+    }
+
 }
